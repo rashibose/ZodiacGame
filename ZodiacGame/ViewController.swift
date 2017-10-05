8b77d4f46b3c5d0fb080fedb026f45aaae26d682
Rashi Bose <rbose@Rashis-MacBook-Pro.local>
2017-10-05 15:19:19 -0700

    Initial Commit

---
+++ ZodiacGame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ZodiacGame
+//
+//  Created by Rashi Bose on 10/5/17.
+//  Copyright © 2017 Rashi Bose. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
